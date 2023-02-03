cd0f147c6946659c24700d9a9db799077eaed5fd
saengle <zohv817@gmail.com>
2023-02-03 22:31:16 +0900

    Add: Actual air status on screen

--- lib/main_screen.dart
+++ lib/main_screen.dart
@@ -1,4 +1,10 @@
+import 'dart:async';
+import 'dart:convert';
+
+import 'package:fine_dust_app/api_key.dart';
+import 'package:fine_dust_app/model/air_result.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class MainScreen extends StatefulWidget {
   const MainScreen({Key? key}) : super(key: key);
@@ -8,72 +14,154 @@ class MainScreen extends StatefulWidget {
 }
 
 class _MainScreenState extends State<MainScreen> {
+  AirResult? _result;
+  final _apiKey = ApiKey();
+
+  Future<AirResult> fetchData() async {
+    var url = Uri.https(
+        'api.airvisual.com', '/v2/nearest_city', {'key': _apiKey.key});
+
+    var response = await http.get(url);
+    AirResult result = AirResult.fromJson(json.decode(response.body));
+    return result;
+  }
+
+  @override
+  void initState() {
+    fetchData().then((airResult) {
+      setState(() {
+        _result = airResult;
+      });
+    });
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('미세먼지 앱'),
-        ),
-        body: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            const SizedBox(height: 220),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Text(
-                '현재 위치 미세먼지',
-                style: TextStyle(fontSize: 30),
-              ),
-            ),
-            Card(
-              child: Column(
-                children: [
-                  Container(
-                    child: Container(
-                      height: 70,
-                      color: Colors.yellow,
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: [
-                          Text('이모티콘'),
-                          Text(
-                            '80',
-                            style: TextStyle(
-                                fontWeight: FontWeight.bold, fontSize: 30),
-                          ),
-                          Text(
-                            '보통',
-                            style: TextStyle(fontSize: 18),
-                          ),
-                        ],
+      appBar: AppBar(
+        title: const Text('미세먼지 앱'),
+      ),
+      // ignore: unnecessary_null_comparison
+      body: _result == null
+          ? const Center(
+              child: CircularProgressIndicator(),
+            )
+          : Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const SizedBox(height: 220),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(
+                    '${_result!.data.city} 미세먼지',
+                    style: const TextStyle(fontSize: 30),
+                  ),
+                ),
+                Card(
+                  child: Column(
+                    children: [
+                      Container(
+                        height: 70,
+                        color: getColor(_result!),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            const Text(
+                              '미세먼지 농도',
+                              style: TextStyle(fontSize: 18),
+                            ),
+                            Text(
+                              '${_result!.data.current.pollution.aqius}',
+                              style: const TextStyle(
+                                  fontWeight: FontWeight.bold, fontSize: 30),
+                            ),
+                            Text(
+                              getWeatherStatus(_result!),
+                              style: const TextStyle(fontSize: 18),
+                            ),
+                          ],
+                        ),
                       ),
-                    ),
+                      SizedBox(
+                        height: 50,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            getIcon(_result!, getColor(_result!)),
+                            Text('습도 ${_result!.data.current.weather.hu}%'),
+                            Text('풍속 ${_result!.data.current.weather.ws}m/s'),
+                          ],
+                        ),
+                      ),
+                    ],
                   ),
-                  Container(
-                    height: 50,
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        Text('이미지'),
-                        Text('습도 : 100%'),
-                        Text('풍속 3.7m/s'),
-                      ],
-                    ),
+                ),
+                ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.orange,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20))),
+                  child: const Icon(
+                    Icons.refresh,
                   ),
-                ],
-              ),
+                ),
+              ],
             ),
-            ElevatedButton(
-              onPressed: () {},
-              style: ElevatedButton.styleFrom(
-                  primary: Colors.orange,
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(20))),
-              child: const Icon(
-                Icons.refresh,
-              ),
-            ),
-          ],
-        ));
+    );
+  }
+
+  Color getColor(AirResult result) {
+    Color resultColor = Colors.black;
+    if (result.data.current.pollution.aqius <= 50) {
+      return Colors.green;
+    } else if (result.data.current.pollution.aqius <= 100) {
+      return Colors.yellow;
+    } else if (result.data.current.pollution.aqius <= 150) {
+      return Colors.orange;
+    } else {
+      return Colors.red;
+    }
+  }
+
+  String getWeatherStatus(AirResult result) {
+    if (result.data.current.pollution.aqius <= 50) {
+      return '좋음';
+    } else if (result.data.current.pollution.aqius <= 100) {
+      return '보통';
+    } else if (result.data.current.pollution.aqius <= 150) {
+      return '나쁨';
+    } else {
+      return '최악';
+    }
+  }
+
+  Icon getIcon(AirResult result, Color resultColor) {
+    if (result.data.current.pollution.aqius <= 50) {
+      return Icon(
+        Icons.sentiment_satisfied,
+        color: resultColor,
+        size: 40,
+      );
+    } else if (result.data.current.pollution.aqius <= 100) {
+      return Icon(
+        Icons.sentiment_satisfied,
+        color: resultColor,
+        size: 40,
+      );
+    } else if (result.data.current.pollution.aqius <= 150) {
+      return Icon(
+        Icons.sentiment_dissatisfied,
+        color: resultColor,
+        size: 40,
+      );
+    } else {
+      return Icon(
+        Icons.sentiment_dissatisfied,
+        color: resultColor,
+        size: 40,
+      );
+    }
   }
 }
--- lib/model/air_result.dart
+++ lib/model/air_result.dart
@@ -1,154 +1,169 @@
 class AirResult {
-  final String? status;
-  final Data? data;
-
   AirResult({
-    this.status,
-    this.data,
+    required this.status,
+    required this.data,
   });
+  late final String status;
+  late final Data data;
 
-  AirResult.fromJson(Map<String, dynamic> json)
-      : status = json['status'] as String?,
-        data = (json['data'] as Map<String, dynamic>?) != null
-            ? Data.fromJson(json['data'] as Map<String, dynamic>)
-            : null;
+  AirResult.fromJson(Map<String, dynamic> json) {
+    status = json['status'];
+    data = Data.fromJson(json['data']);
+  }
 
-  Map<String, dynamic> toJson() => {'status': status, 'data': data?.toJson()};
+  Map<String, dynamic> toJson() {
+    final _data = <String, dynamic>{};
+    _data['status'] = status;
+    _data['data'] = data.toJson();
+    return _data;
+  }
 }
 
 class Data {
-  final String? city;
-  final String? state;
-  final String? country;
-  final Location? location;
-  final Current? current;
-
   Data({
-    this.city,
-    this.state,
-    this.country,
-    this.location,
-    this.current,
+    required this.city,
+    required this.state,
+    required this.country,
+    required this.location,
+    required this.current,
   });
+  late final String city;
+  late final String state;
+  late final String country;
+  late final Location location;
+  late final Current current;
 
-  Data.fromJson(Map<String, dynamic> json)
-      : city = json['city'] as String?,
-        state = json['state'] as String?,
-        country = json['country'] as String?,
-        location = (json['location'] as Map<String, dynamic>?) != null
-            ? Location.fromJson(json['location'] as Map<String, dynamic>)
-            : null,
-        current = (json['current'] as Map<String, dynamic>?) != null
-            ? Current.fromJson(json['current'] as Map<String, dynamic>)
-            : null;
+  Data.fromJson(Map<String, dynamic> json) {
+    city = json['city'];
+    state = json['state'];
+    country = json['country'];
+    location = Location.fromJson(json['location']);
+    current = Current.fromJson(json['current']);
+  }
 
-  Map<String, dynamic> toJson() => {
-        'city': city,
-        'state': state,
-        'country': country,
-        'location': location?.toJson(),
-        'current': current?.toJson()
-      };
+  Map<String, dynamic> toJson() {
+    final _data = <String, dynamic>{};
+    _data['city'] = city;
+    _data['state'] = state;
+    _data['country'] = country;
+    _data['location'] = location.toJson();
+    _data['current'] = current.toJson();
+    return _data;
+  }
 }
 
 class Location {
-  final String? type;
-  final List<double>? coordinates;
-
   Location({
-    this.type,
-    this.coordinates,
+    required this.type,
+    required this.coordinates,
   });
+  late final String type;
+  late final List<double> coordinates;
 
-  Location.fromJson(Map<String, dynamic> json)
-      : type = json['type'] as String?,
-        coordinates = (json['coordinates'] as List?)
-            ?.map((dynamic e) => e as double)
-            .toList();
+  Location.fromJson(Map<String, dynamic> json) {
+    type = json['type'];
+    coordinates = List.castFrom<dynamic, double>(json['coordinates']);
+  }
 
-  Map<String, dynamic> toJson() => {'type': type, 'coordinates': coordinates};
+  Map<String, dynamic> toJson() {
+    final _data = <String, dynamic>{};
+    _data['type'] = type;
+    _data['coordinates'] = coordinates;
+    return _data;
+  }
 }
 
 class Current {
-  final Pollution? pollution;
-  final Weather? weather;
-
   Current({
-    this.pollution,
-    this.weather,
+    required this.pollution,
+    required this.weather,
   });
+  late final Pollution pollution;
+  late final Weather weather;
 
-  Current.fromJson(Map<String, dynamic> json)
-      : pollution = (json['pollution'] as Map<String, dynamic>?) != null
-            ? Pollution.fromJson(json['pollution'] as Map<String, dynamic>)
-            : null,
-        weather = (json['weather'] as Map<String, dynamic>?) != null
-            ? Weather.fromJson(json['weather'] as Map<String, dynamic>)
-            : null;
+  Current.fromJson(Map<String, dynamic> json) {
+    pollution = Pollution.fromJson(json['pollution']);
+    weather = Weather.fromJson(json['weather']);
+  }
 
-  Map<String, dynamic> toJson() =>
-      {'pollution': pollution?.toJson(), 'weather': weather?.toJson()};
+  Map<String, dynamic> toJson() {
+    final _data = <String, dynamic>{};
+    _data['pollution'] = pollution.toJson();
+    _data['weather'] = weather.toJson();
+    return _data;
+  }
 }
 
 class Pollution {
-  final String? ts;
-  final int? aqius;
-  final String? mainus;
-  final int? aqicn;
-  final String? maincn;
-
   Pollution({
-    this.ts,
-    this.aqius,
-    this.mainus,
-    this.aqicn,
-    this.maincn,
+    required this.ts,
+    required this.aqius,
+    required this.mainus,
+    required this.aqicn,
+    required this.maincn,
   });
+  late final String ts;
+  late final int aqius;
+  late final String mainus;
+  late final int aqicn;
+  late final String maincn;
 
-  Pollution.fromJson(Map<String, dynamic> json)
-      : ts = json['ts'] as String?,
-        aqius = json['aqius'] as int?,
-        mainus = json['mainus'] as String?,
-        aqicn = json['aqicn'] as int?,
-        maincn = json['maincn'] as String?;
+  Pollution.fromJson(Map<String, dynamic> json) {
+    ts = json['ts'];
+    aqius = json['aqius'];
+    mainus = json['mainus'];
+    aqicn = json['aqicn'];
+    maincn = json['maincn'];
+  }
 
-  Map<String, dynamic> toJson() => {
-        'ts': ts,
-        'aqius': aqius,
-        'mainus': mainus,
-        'aqicn': aqicn,
-        'maincn': maincn
-      };
+  Map<String, dynamic> toJson() {
+    final _data = <String, dynamic>{};
+    _data['ts'] = ts;
+    _data['aqius'] = aqius;
+    _data['mainus'] = mainus;
+    _data['aqicn'] = aqicn;
+    _data['maincn'] = maincn;
+    return _data;
+  }
 }
 
 class Weather {
-  final String? ts;
-  final int? tp;
-  final int? pr;
-  final int? hu;
-  final double? ws;
-  final int? wd;
-  final String? ic;
-
   Weather({
-    this.ts,
-    this.tp,
-    this.pr,
-    this.hu,
-    this.ws,
-    this.wd,
-    this.ic,
+    required this.ts,
+    required this.tp,
+    required this.pr,
+    required this.hu,
+    required this.ws,
+    required this.wd,
+    required this.ic,
   });
+  late final String ts;
+  late final int tp;
+  late final int pr;
+  late final int hu;
+  late final double ws;
+  late final int wd;
+  late final String ic;
 
-  Weather.fromJson(Map<String, dynamic> json)
-      : ts = json['ts'] as String?,
-        tp = json['tp'] as int?,
-        pr = json['pr'] as int?,
-        hu = json['hu'] as int?,
-        ws = json['ws'] as double?,
-        wd = json['wd'] as int?,
-        ic = json['ic'] as String?;
+  Weather.fromJson(Map<String, dynamic> json) {
+    ts = json['ts'];
+    tp = json['tp'];
+    pr = json['pr'];
+    hu = json['hu'];
+    ws = json['ws'];
+    wd = json['wd'];
+    ic = json['ic'];
+  }
 
-  Map<String, dynamic> toJson() =>
-      {'ts': ts, 'tp': tp, 'pr': pr, 'hu': hu, 'ws': ws, 'wd': wd, 'ic': ic};
+  Map<String, dynamic> toJson() {
+    final _data = <String, dynamic>{};
+    _data['ts'] = ts;
+    _data['tp'] = tp;
+    _data['pr'] = pr;
+    _data['hu'] = hu;
+    _data['ws'] = ws;
+    _data['wd'] = wd;
+    _data['ic'] = ic;
+    return _data;
+  }
 }
